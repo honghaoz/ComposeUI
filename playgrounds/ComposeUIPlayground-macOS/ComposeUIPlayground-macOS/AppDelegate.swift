e495ae50867ac918deafcaf9aa6a995d45d6e058
Honghao Zhang <m@honghao.dev>
2024-11-02 09:32:39 -0700

    [playground] manual window

--- playgrounds/ComposeUIPlayground-macOS/ComposeUIPlayground-macOS/AppDelegate.swift
+++ playgrounds/ComposeUIPlayground-macOS/ComposeUIPlayground-macOS/AppDelegate.swift
@@ -33,8 +33,29 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+  private var window: NSWindow?
+  
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
+
+    // create the window
+    let window = NSWindow(
+        contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
+        styleMask: [.titled, .closable, .miniaturizable, .resizable],
+        backing: .buffered,
+        defer: false
+    )
+
+    window.contentViewController = ViewController()
+
+    // configure window
+    window.center()
+    window.setFrameAutosaveName("main-window")
+    window.title = "ComposeUI Playground"
+
+    // make window visible
+    window.makeKeyAndOrderFront(nil)
+
+    self.window = window
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
