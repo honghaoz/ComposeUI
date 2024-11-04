491fd58e119483e3861bd65d23a20992fd564419
Honghao Zhang <m@honghao.dev>
2024-11-04 08:48:02 -0800

    avoid disappeared zero size views

--- ComposeUI/Sources/ComposeUI/ComposeNodes/LabelNode.swift
+++ ComposeUI/Sources/ComposeUI/ComposeNodes/LabelNode.swift
@@ -97,7 +97,7 @@ public struct LabelNode: ComposeNode, FixedSizableComposeNode {
 
   public func viewItems(in visibleBounds: CGRect) -> [ViewItem<View>] {
     let frame = CGRect(origin: .zero, size: size)
-    guard visibleBounds.actuallyIntersects(frame) else {
+    guard visibleBounds.intersects(frame) else {
       return []
     }
 
