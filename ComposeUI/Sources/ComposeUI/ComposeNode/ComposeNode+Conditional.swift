9b8ef85bc219bee81994ab6d0b746c8f31a800b0
Honghao Zhang <m@honghao.dev>
2024-11-05 22:21:34 -0800

    add conditional operator tests

--- ComposeUI/Sources/ComposeUI/ComposeNode/ComposeNode+Conditional.swift
+++ ComposeUI/Sources/ComposeUI/ComposeNode/ComposeNode+Conditional.swift
@@ -1,5 +1,5 @@
 //
-//  ComposeNode+If.swift
+//  ComposeNode+Conditional.swift
 //  ComposeUI
 //
 //  Created by Honghao Zhang on 9/29/24.
@@ -30,28 +30,26 @@
 
 import Foundation
 
-extension ComposeNode {
+public extension ComposeNode {
 
-  /// If the condition is true, then return the result of the then closure.
-  /// Otherwise, return the original node.
+  /// If the condition is true, then return the result of the then closure. Otherwise, return the original node.
   ///
   /// - Parameters:
   ///   - condition: The condition to check.
   ///   - then: The closure to evaluate if the condition is true.
   /// - Returns: A new node with the result of the then closure if the condition is true, otherwise the original node.
-  func `if`(_ condition: Bool, then: (Self) -> some ComposeNode) -> any ComposeNode {
-    condition ? then(self) : self
+  func `if`(_ condition: Bool, then: (Self) -> any ComposeNode) -> any ComposeNode {
+    return condition ? then(self) : self
   }
 
-  /// If the condition is true, then return the result of the then closure.
-  /// Otherwise, return the result of the else closure.
+  /// If the condition is true, then return the result of the then closure. Otherwise, return the result of the else closure.
   ///
   /// - Parameters:
   ///   - condition: The condition to check.
   ///   - then: The closure to evaluate if the condition is true.
   ///   - else: The closure to evaluate if the condition is false.
   /// - Returns: A new node with the result of the then closure if the condition is true, otherwise the result of the else closure.
-  func `if`(_ condition: Bool, then: (Self) -> some ComposeNode, else: (Self) -> some ComposeNode) -> any ComposeNode {
+  func `if`(_ condition: Bool, then: (Self) -> any ComposeNode, else: (Self) -> any ComposeNode) -> any ComposeNode {
     condition ? then(self) : `else`(self)
   }
 }
